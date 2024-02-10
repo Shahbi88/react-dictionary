be2107782ec0ca3c5f78e3c085d257bf7cc18c0d
Shahbi88 <rihabazar@gmail.com>
2024-02-10 18:49:32 +0000

    axios working

--- src/dictionary.js
+++ src/dictionary.js
@@ -1,10 +1,18 @@
 import React, { useState } from "react";
-
+import axios from "axios";
 export default function Dictionary() {
   let [keyWord, setKeyword] = useState("");
+
+  function handleResponse(result) {
+    console.log(result.data.meanings[0]);
+  }
   function search(event) {
     event.preventDefault();
     alert(`searching for ${keyWord}`);
+    let apiKey = "d9833o0d576c1t0baaff4f33407e3baa";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyWord}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
