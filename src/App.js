8838daf31caacea3d13b3da118866b4aad112351
Shahbi88 <rihabazar@gmail.com>
2024-02-24 08:16:31 +0000

    netlify

--- src/App.js
+++ src/App.js
@@ -18,9 +18,11 @@ function App() {
         </div>
         <small>
           <p className="footer">
-            Coded by <a href="https://www.rihabazar.co.uk/">Rihab Azar</a> and
-            is open-sourced on{" "}
-            <a href="https://github.com/Shahbi88/react-dictionary">Github</a>
+            Coded by <a href="https://www.rihabazar.co.uk/">Rihab Azar</a> , is
+            open-sourced on{" "}
+            <a href="https://github.com/Shahbi88/react-dictionary">Github</a>{" "}
+            and hosted on{" "}
+            <a href="https://sunny-belekoy-ac520e.netlify.app/">Netlify</a>
           </p>
         </small>
       </header>
